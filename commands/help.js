8afe6028c304297d49b70435b1d2d42a8baf248b
ReTDevelopment <returnedtrulse@gmail.com>
2024-01-21 21:05:47 -0500

    'Creation'

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,5 @@
 const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
-const fs = require('node:fs');
-const path = require('node:path');
-const commandPath = __dirname;
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
-let commands = []
-
-for (const file of commandFiles) {
-  const command = require(`./${file}`);
-  if (file != "help.js") {
-    commands.push(command)
-  }
-}
 module.exports = {
   enabled: true,
   developerOnly: false,
@@ -23,27 +11,6 @@ module.exports = {
     .setDescription('Gets a list of commands!'),
 
   async execute(client, interaction, roblox, events) {
-    if (this.enabled == false) return;
-    if(this.developerOnly == true && interaction.user.id != "1111485486607892562") return;
-    
-    const verifyEmbed = new EmbedBuilder()
-        .setColor(0xff3224)
-        .setDescription(`## Command List\n\nThe following is a list of commands available to use!\n* ⚪ Enabled and unlocked command.\n* ⚫ Disabled and unlocked command.\n* 🔒 Developer only command.\n* 🔐 Unlocked by **linking your account.**`)
-        .setFooter({text: 'AHA Commands'})
-        .setTimestamp()
-      for(let i = 0; i < commands.length; i++) {
-        let command = commands[i];
-        let enabledIcon; if(command.enabled == true) {enabledIcon = "⚪"} else{enabledIcon = "⚫"}
-        let opt = [];
-        let options = command.options.forEach(o => {
-          opt.push("``" + o + "``")
-        })
-        opt = opt.join(" ")
-        if(command.linkLock == true) {enabledIcon = "🔐"}
-        if(command.developerOnly == true) {enabledIcon = "🔒"}
-        
-        verifyEmbed.addFields({ name: `${enabledIcon} ${command.data.name} ${opt}`, value: command.data.description, inline: false})
-      }
-      interaction.reply({content: "", embeds: [verifyEmbed], ephemeral: true})
+    interaction.reply({content: `Working!`})
   }
 }
--- index.js
+++ index.js
@@ -31,52 +31,6 @@ for (const file of commandFiles) {
   }
 }
 
-var blacklistedWords = ["UltimeBob1", "hacking", "tubers93", "v3rmillion", "vermillion", "edgelord", "c00lkidd", "coolkidd", "tubers", "hack", "haker", "hacker", "bunbun", "hax", "1x1", "2x2", "3x3", "testbot", "anonymous", "johndoe", "john_doe", "janedoe", "jane_doe", "snypase", "jjsploit", "krnl", "sk8r", "hacke", "hacking", "expro", "exploiter", "exploit", "coolkid", "666", "coolkid", "666", "guest"];
-var whitelistedGroupIds = [201622, 32392862, 32452168];
-
-function checkForBlacklist(str) {
-  for (let word in blacklistedWords) {
-    if (str.toString().toLowerCase().includes(blacklistedWords[word].toString().toLowerCase())) {
-      return true
-    }
-  }
-  return false
-}
-
-function checkGroups(groups) {
-  for (let group in groups) {
-let groupData = groups[group]
-    if(checkForBlacklist(groupData.Name) == true && !whitelistedGroupIds.includes(groupData.Id))
-    {
-return true
-    }  
-  } 
-return false
-}
-
- async function doAutomation(robloxC) {
-  if(!robloxC) return;
-  let group = 201622
-  let checkRoles = {awaiting: 1122034, jailed: 5309509, class: 1113148}
-  let users = await noblox.getPlayers(201622, checkRoles.awaiting)
-  for (let user in users) {
-      let userInfo = users[user];
-      let checkName = checkForBlacklist(userInfo.username) || checkForBlacklist(userInfo.displayName)
-      let roles = await noblox.getGroups(userInfo.userId)
-      let checkGroup = checkGroups(roles)
-      let blacklist = checkGroup == true || checkName == true
-      if(blacklist == true) {
-        let res = await noblox.setRank(201622, userInfo.userId, checkRoles.jailed)
-      }else{
-        let res = await noblox.setRank(201622, userInfo.userId, checkRoles.class)
-    }
-  }
-}
-
-
-
-console.clear();
-
 let robloxC; 
 
 client.once(Events.ClientReady, async c => {
@@ -90,17 +44,9 @@ client.once(Events.ClientReady, async c => {
   }
 
   let currentUser;
-  try{currentUser = await noblox.setCookie(process.env.COOKIE.toString()); doAutomation(currentUser); 
-    setInterval(async function() {
-      console.log("Promotion inbound")
-      doAutomation(currentUser)
-    }, 120000)
+  try{
+    currentUser = await noblox.setCookie(process.env.COOKIE.toString()); 
   }catch(e){console.log}
-  let currentEvents;
-  try{currentEvents= await noblox.setCookie(process.env.EVENTS.toString()); 
-  }catch(e) {
-    console.log(`Could not log into the events account!`)
-  }
   console.log(`Ready, logged in as ${c.user.tag}!`)
 
   const express = require("express");
@@ -117,38 +63,7 @@ client.once(Events.ClientReady, async c => {
   app.listen(4000);
 })
 
-let blisted = ["1134805838050103316"]
-
-client.on(Events.GuildMemberAdd, async member => {
-  if(blisted.includes(member.id.toString())) return member.kick("Blacklisted.");
-  let role = member.guild.roles.cache.find(r => r.id === "1078503125213790318")
-  member.roles.add(role)
-})
-
 client.on(Events.InteractionCreate, async interaction => {
-  if(interaction.isModalSubmit()) {
-    if(interaction.customId.toString().includes("reportModal")) {
-      let type = interaction.customId.toString().split("-")[1] || "N/A"
-      
-      const reporting = interaction.fields.getTextInputValue('reporting') || "No reported was provided.";
-      const reportDescription = interaction.fields.getTextInputValue('reportDescription') || "No description was provided.";
-      const links = interaction.fields.getTextInputValue('reportLinks') || "N/A";
-      const evidence = interaction.fields.getTextInputValue('reportEvidence') || "N/A";
-
-
-      let username = interaction.member.id
-      let channel;
-      if(type == "exploiter") {channel = "732566735772712965"}else if(type == "group") {channel = "927362457473802351"}else if(type == "scammer") {channel = "737306541048463380"}
-
-      if (channel != null) {
-        client.channels.cache.get(channel).send({
-           content: "```A new report has been submitted!```" + `\n> * ` + "``Who?:``" + ` ${reporting}\n> * ` + "``Relevant links?:``" + ` ${links}\n> * ` + "``Evidence?:``" + ` ${evidence}\n> * ` + "``Report Description``: " + `${reportDescription}\n\n > Members will not be able to post here by default. If you are a Council member and want more information, then please contact them via their direct messages. ||<@${interaction.user.id}>||.\n\n> If you are the original reporter, then please wait while we investigate your report. If we find it to be a valid report, then your post will be approved and further instructions will be given. Remember that false reports will result in potential moderation action.\n\n` + "```Thank you for submitting this report!```",
-        }).then(async function() {
-          await interaction.reply({content: "Successfully submitted!", ephemeral: true})
-        })
-    }}
-  }
-  
   if(!interaction.isChatInputCommand()) return;
   const command = interaction.client.commands.get(interaction.commandName);
   if(!command) {
@@ -160,7 +75,7 @@ client.on(Events.InteractionCreate, async interaction => {
     if (command.enabled == false) return;
       if(command.developerOnly == true && interaction.user.id != "1111485486607892562") return await interaction.reply({content: 'Sorry, but you do not have permission to use this command!', ephemeral: true});
       if(command.disableDM == true && interaction.guildId == null) return await interaction.reply({content: 'Sorry, but you can only use this command in servers!', ephemeral: true});
-    await command.execute(client, interaction, noblox, events)
+    await command.execute(client, interaction, noblox)
   }catch(e) {
     console.log(e)
     if(interaction.replied || interaction.deferred) {
